3f7a049543c7aef4112e61d8facc7bfe59102dd2
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2025-04-06 22:54:40 +0200

    exercise1

---
+++ exercise1.py
@@ -0,0 +1,34 @@
+"""
+Exercise: Download and Deprocess Data
+
+| Code                                                     | Description                                                                   |
+| `run_path('myscript.py')`                                | Run a script in the current python session                                    |
+| `run_path('myscript.py', init_globals={'a': 3, 'b': 4})` | Run a script, making variables `a` and `b` with specific value from the start |
+
+Goal: Use `run_path()` to run two key scripts in the workflow, supplying the relevant variables to each script.
+
+"""
+
+# %%
+import os  
+from pathlib import Path
+from runpy import run_path
+
+from tqdm import tqdm  # makes progress bars
+
+os.makedirs('./data', exist_ok=True)
+
+# %% Run Download Data Script: 1_download_data.py
+url = "https://uni-bonn.sciebo.de/s/Po9q3wLiNXTxgbj"
+nfiles = 3
+
+# run_path() here
+
+
+
+# %%  Run Deprocessing Script, once on each file: 2_deprocess_session.py
+
+files = list(Path('./data/original').glob('*.nc'))
+for file in tqdm(files, desc=f"Generating Raw Session Files"):
+    # run_path() here
+    
